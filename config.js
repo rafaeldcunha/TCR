48fbf210921b54cdfa91ac7035d0c038417385b0
rafaeldcunha <88018335+rafaeldcunha@users.noreply.github.com>
2026-08-03 10:16:44 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.TCR_CONFIG = {
   url: "https://xisjwlmwhsziwbcwkhil.supabase.co",
-  key: "SUA_CHAVE_ANON_AQUI"
+  key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhpc2p3bG13aHN6aXdiY3draGlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU2ODIzMDcsImV4cCI6MjEwMTI1ODMwN30.oagLeGXy_YLZT3lHLqSTHo3EZP-9oLMijcoD0BMaf0Q"
 };
